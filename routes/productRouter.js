f360c8371939b78ee42cf88e643b1d16fe09e01b
chinch2 <johao5991@gmail.com>
2019-12-02 16:24:48 -0400

    Express REST API with Mongoose Part 2

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -92,4 +92,172 @@ productRouter.route("/")
             .catch((err) => next(err));
     });
 
+productRouter.route("/:prodId/comments/:commentId")
+    .get((req, res, next) => {
+        Shirts.findById(req.params.prodId)
+            .then((shirt) => {
+                if (shirt != null && shirt.comments.id(req.params.commentId) != null) {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(shirt.comments.id(req.params.commentId));
+                }
+                else if (shirt == null) {
+                    let err = new Error('Shirt ' + req.params.prodId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                } else {
+                    let err = new Error('Comment ' + req.params.commentId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+            }, (err) => {
+                next(err);
+            })
+            .catch((err) => next(err));
+    })
+
+    .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end("POST operation not supported on /products/" + req.params.prodId +
+        "/comments/" + req.params.commentId);
+    })
+
+    .put((req, res, next) => {
+        Shirts.findById(req.params.prodId)
+            .then((shirt) => {
+                if (shirt != null && shirt.comments.id(req.params.commentId) != null) {
+                    if (req.body.rating) {
+                        shirt.comments.id(req.params.commentId).rating = req.body.rating;
+                    }
+                    if(req.body.comment) {
+                        shirt.comments.id(req.params.commentId).comment = req.body.comment;
+                    }
+                    shirt.save()
+                        .then((shirt) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(shirt);
+                        })
+                }
+                else if (shirt == null) {
+                    let err = new Error('Shirt ' + req.params.prodId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                } else {
+                    let err = new Error('Comment ' + req.params.commentId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+            }, (err) => {
+                next(err);
+            })
+            .catch((err) => next(err));
+    })
+
+    .delete((req, res, next) => {
+        Shirts.findById(req.params.prodId)
+            .then((shirt) => {
+                if (shirt != null && shirt.comments.id(req.params.commentId) != null) {
+                    shirt.comments.id(req.params.commentId).remove();
+                    shirt.save()
+                        .then((shirt) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(shirt);
+                        }, (err) => {
+                            next(err);
+                        })
+                        .catch ((err) => next(err));
+                }
+                else if (shirt == null) {
+                    let err = new Error('Shirt ' + req.params.prodId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                } else {
+                    let err = new Error('Comment ' + req.params.commentId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+            }, (err) => {
+                next(err);
+            })
+            .catch((err) => next(err));
+    });
+
+productRouter.route("/:prodId/comments")
+    .get((req, res, next) => {
+        Shirts.findById(req.params.prodId)
+            .then((shirt) => {
+                if (shirt != null) {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(shirt.comments);
+                }
+                else {
+                    let err = new Error('Shirt ' + req.params.prodId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+            }, (err) => {
+                next(err);
+            })
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        Shirts.findById(req.params.prodId)
+            .then((shirt) => {
+                if (shirt != null) {
+                    shirt.comments.push(req.body);
+                    shirt.save()
+                        .then((shirt) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(shirt);
+                        }, (err) => {
+                            next(err);
+                        })
+                        .catch((err) => next(err));
+                }
+                else {
+                    let err = new Error('Shirt ' + req.params.prodId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+            }, (err) => {
+                next(err);
+            })
+            .catch((err) => next(err));
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end("PUT operation not supported on /products/" + req.params.prodId + "/comments");
+    })
+    .delete((req, res, next) => {
+        Shirts.findById(req.params.prodId)
+            .then((shirt) => {
+                if (shirt != null) {
+                    for (let i = (shirt.comments.length - 1); i >= 0; i--) {
+                        shirt.comments.id(shirt.comments[i]._id).remove();
+                    }
+                    shirt.save()
+                        .then((shirt) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(shirt);
+                        }, (err) => {
+                            next(err);
+                        })
+                        .catch ((err) => next(err));
+                }
+                else {
+                    let err = new Error('Shirt ' + req.params.prodId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+            }, (err) => {
+                next(err);
+            })
+            .catch((err) => next(err));
+    });
+
 module.exports = productRouter;
